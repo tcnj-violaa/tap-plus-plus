6ee130fd9864aba30004ba98ea72162dc74a87e1
Raymonf <raymonf@outlook.com>
2021-04-25 23:50:10 -0400

    sail

--- resources/views/audio/edit_request.blade.php
+++ resources/views/audio/edit_request.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.app')
 @section('content')
     <div class="container tw-max-w-4xl">
-        <div class="form-group text-center mt-5">
-            <select class="form-control bg-light p-4 tw-text-black mb-0
-                {!! nl2br(e($audio->transcript_text)) !!}
-            </select>
-        </div>
+        <form>
+            <div class="mt-5 mb-2 tw-font-serif tw-text-lg pr-0" style="min-height: 500px; max-height: calc(100vh - 150px); overflow-y: auto">
+                <div class="form-group text-center mt-5">
+                    <textarea id="transcriptEditText" class="form-control bg-light p-4 tw-text-black mb-0">
+                        {!! nl2br(e($audio->transcript_text)) !!}
+                    </textarea>
+                </div>
+            </div>
+        </form>
     </div>
 @endsection
