6624aae572ac7aa465e89f2313cd6cfb7ae26a14
Raymonf <raymonf@outlook.com>
2021-04-26 00:24:40 -0400

    wip: seeders for dev vm

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,15 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
+    private function updateIdentity(string $table, string $primaryKey = 'id')
+    {
+        DB::statement("SELECT setval(pg_get_serial_sequence('$table', '$primaryKey'), (SELECT MAX($primaryKey) FROM $table));");
+    }
+
     /**
      * Seed the application's database.
      *
@@ -14,5 +20,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(UsersTableSeeder::class);
+        $this->call(AudioTableSeeder::class);
+        $this->call(TagCategoryTableSeeder::class);
+        $this->call(TagsTableSeeder::class);
+        $this->call(AudioTagTableSeeder::class);
+        $this->call(TranscriptsTableSeeder::class);
+        $this->call(UserEditRequestTableSeeder::class);
+        $this->call(AuditLogsTableSeeder::class);
+
+        $this->updateIdentity('users');
+        $this->updateIdentity('audio');
+        $this->updateIdentity('tag_category');
+        $this->updateIdentity('tags');
+        $this->updateIdentity('audio_tag');
+        $this->updateIdentity('transcripts');
+        $this->updateIdentity('user_edit_request');
     }
 }
--- resources/views/audio/edit_request.blade.php
+++ resources/views/audio/edit_request.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container tw-max-w-4xl">
+    <div class="container tw-max-w-5xl">
         <div class="row">
             <div class="col">
                 <div class="jumbotron container p-4 mb-0 mt-5">
@@ -9,7 +9,7 @@
                 <form>
                     <div class="mt-2 mb-2 tw-font-serif tw-text-lg pr-0">
                         <div class="form-group">
-                            <textarea id="transcriptOrigText" class="form-control form-control-sm bg-light p-4 tw-text-black mb-0 bg-secondary" rows="30" style="resize: none; min-height: 500px; max-height: calc(100vh - 150px); overflow-y: auto" readonly>{!! e($audio->transcript_text) !!}</textarea>
+                            <textarea id="transcriptOrigText" class="form-control bg-light p-4 tw-text-black mb-0 bg-secondary" rows="25" style="resize: none; min-height: 500px; max-height: calc(100vh - 150px); overflow-y: auto" readonly>{!! e($audio->transcript_text) !!}</textarea>
                         </div>
                     </div>
                 </form>
@@ -21,16 +21,15 @@
                 <form>
                     <div class="mt-2 mb-2 tw-font-serif tw-text-lg pr-0">
                         <div class="form-group">
-                            <textarea id="transcriptEditText" class="form-control form-control-sm bg-light p-4 tw-text-black mb-0" style="resize: none; min-height: 500px; max-height: calc(100vh - 150px); overflow-y: auto" rows="30">{!! e($audio->transcript_text) !!}</textarea>
+                            <textarea id="transcriptEditText" class="form-control bg-light p-4 tw-text-black mb-0 text-success" style="resize: none; min-height: 500px; max-height: calc(100vh - 150px); overflow-y: auto" rows="25">{!! e($audio->transcript_text) !!}</textarea>
                         </div>
                     </div>
               </form>
             </div>
         </div>
         <div class="row">
-
-            <div class="container p-3 mb-0 mt-5">
-                <button class="btn btn-lg btn-primary btn-block mt-2 mb-2" type="submit">Submit Request</button>
+            <div class="container p-3">
+                <button class="btn btn-lg btn-primary btn-block mt-1 mb-2" type="submit">Submit Request</button>
             </div>
         </div>
     </div>
